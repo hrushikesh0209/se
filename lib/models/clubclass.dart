2d97f0c11869dfbed272112a9bb794b40d1467d2
hrushikesh0209 <hrushikesh.0902@gmail.com>
2023-05-02 13:27:40 +0530

    Final submission

--- lib/models/clubclass.dart
+++ lib/models/clubclass.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart.html'
-class Clubs {
 
+class Clubs {
   final String name;
   final String clubhead;
   final String headmail;
@@ -12,16 +11,24 @@ class Clubs {
   final String description;
   String id;
   String list1;
-  Clubs({ required this.name,required this.id, required this.headmail,required this.headphone,required this.url,required this.description,required this.clubhead,required this.list1 });
+  Clubs(
+      {required this.name,
+      required this.id,
+      required this.headmail,
+      required this.headphone,
+      required this.url,
+      required this.description,
+      required this.clubhead,
+      required this.list1});
 
   Map<String, dynamic> tojson() => {
-    'name': name,
-    'id': id,
-    'headmail': headmail,
-    'headphone': headphone,
-    'url': url,
-    'description': description,
-    'clubhead': clubhead,
-    'list1':list1,
-  };
+        'name': name,
+        'id': id,
+        'headmail': headmail,
+        'headphone': headphone,
+        'url': url,
+        'description': description,
+        'clubhead': clubhead,
+        'list1': list1,
+      };
 }
